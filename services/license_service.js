c78725466bf5021aa832d37b1f091fad6829cd42
Christian Görg <christian.goerg@de.trumpf.com>
2017-03-13 13:58:23 +0100

    License service changes

--- services/license_service.js
+++ services/license_service.js
@@ -13,28 +13,27 @@ var LicenseService = function () {};
 const license_service = new LicenseService();
 util.inherits(LicenseService, EventEmitter);
 
-socket =  io('http://' + constants.HOST_SETTINGS.MARKETPLACE_CORE.HOST
+license_service.socket =  io.connect('http://' + constants.HOST_SETTINGS.MARKETPLACE_CORE.HOST
     + ":" + constants.HOST_SETTINGS.MARKETPLACE_CORE.PORT +  "/licenses",{transports: ['websocket']});
-socket.connect();
-socket.on('connect', function(){
+license_service.socket.on('connect', function(){
     logger.debug("connected to license SocketIO at Marketplace");
 });
 
-socket.on('disconnect', function () {
+license_service.socket.on('disconnect', function () {
     logger.debug("disconnected from license SocketIO at Marketplace");
 });
 
-socket.on('updateAvailable', function (data) {
+license_service.socket.on('updateAvailable', function (data) {
     license_service.emit('updateAvailable', data.offerId, data.hsmId);
 });
 
 
 license_service.registerUpdates = function (hsmId) {
-    socket.emit('room',hsmId);
+    license_service.socket.emit('room',hsmId);
 };
 
 license_service.unregisterUpdates = function (hsmId) {
-    socket.emit('leave',hsmId);
+    license_service.socket.emit('leave',hsmId);
 };
 
 module.exports = license_service;
