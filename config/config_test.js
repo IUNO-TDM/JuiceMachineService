65192617f0c24de2c42d315436e48d7bcb357a3b
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-07-18 17:28:19 +0200

    added tet config

---
+++ config/config_test.js
@@ -0,0 +1,23 @@
+
+var self = {
+};
+
+// ---- CONFIGURATION EXPORT ----
+self.HOST_SETTINGS = {
+    MARKETPLACE_CORE: {
+        PROTOCOL:'http',
+        HOST: 'test-tdm.fritz.box',
+        PORT: 3002
+    },
+    OAUTH_SERVER: {
+        PROTOCOL:'http',
+        HOST: 'test-tdm.fritz.box',
+        PORT: 3006
+    }
+};
+self.OAUTH_CREDENTIALS = {
+    CLIENT_ID: '210d760f-6222-4f70-8a3c-5d6c8b13ac74',
+    CLIENT_SECRET: 'fe685866-0966-40cc-92c9-004ed96d81f7'
+};
+
+module.exports = self;
