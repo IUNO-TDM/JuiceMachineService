ec3724c1a866b36aa8d5639b31ce6e259e7295ff
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-11-22 14:26:46 +0100

    Refactored websocket authentication fixing timing issues close #48

--- services/license_service.js
+++ services/license_service.js
@@ -13,72 +13,89 @@ const config = require('../config/config_loader');
 const authServer = require('../adapter/auth_service_adapter');
 
 var LicenseService = function () {
+    const self = this;
     this.registeredRooms = {};
+
 };
 
 const license_service = new LicenseService();
 util.inherits(LicenseService, EventEmitter);
 
+license_service.socket = io.connect('http://' + config.HOST_SETTINGS.MARKETPLACE_CORE.HOST
+    + ":" + config.HOST_SETTINGS.MARKETPLACE_CORE.PORT + "/licenses", {
+    transports: ['websocket'],
+    extraHeaders: {},
+    autoConnect: false
+});
 
-authServer.getClientAccessToken(function (err, token) {
-    license_service.socket = io.connect('http://' + config.HOST_SETTINGS.MARKETPLACE_CORE.HOST
-        + ":" + config.HOST_SETTINGS.MARKETPLACE_CORE.PORT + "/licenses", {
-        transports: ['websocket'],
-        extraHeaders: {
-            Authorization: 'Bearer ' + (token ? token.accessToken : '')
-        }
-    });
+license_service.socket.on('connect', function () {
+    logger.debug("[license_service] connected to license SocketIO at Marketplace");
 
-    license_service.socket.on('connect', function () {
-        logger.debug("connected to license SocketIO at Marketplace");
-
-        // register rooms on reconnect
-        Object.keys(license_service.registeredRooms).forEach(function (room) {
-            if (!room) {
-                return;
-            }
-            logger.debug('[license_service] Re-Joining room: ' + room);
-            license_service.socket.emit('room', room);
-        });
-    });
-
-    license_service.socket.on('connect_error', function (error) {
-        logger.debug("[license_client] Connection Error: " + error);
-        authServer.invalidateToken();
-    });
-
-    license_service.socket.on('disconnect', function () {
-        logger.debug("disconnected from license SocketIO at Marketplace");
-    });
-
-    /**
-     * updateAvailable events are directly passed to the registered clients. Via the license service.
-     */
-    license_service.socket.on('updateAvailable', function (data) {
-        license_service.emit('updateAvailable', data.offerId, data.hsmId);
-    });
-
-
-    license_service.registerUpdates = function (hsmId) {
-        /**
-         * The subscriptions for license updates are separated by the hsmId.
-         */
-        if (!hsmId) {
+    // register rooms on reconnect
+    Object.keys(license_service.registeredRooms).forEach(function (room) {
+        if (!room) {
             return;
         }
-
-        license_service.registeredRooms[hsmId] = true;
-
-        license_service.socket.emit('room', hsmId);
-    };
-
-    license_service.unregisterUpdates = function (hsmId) {
-        if (license_service.registeredRooms[hsmId]) {
-            delete  license_service.registeredRooms[hsmId];
-        }
-
-        license_service.socket.emit('leave', hsmId);
-    };
+        logger.debug('[license_service] Re-Joining room: ' + room);
+        license_service.socket.emit('room', room);
+    });
 });
+
+license_service.socket.on('error', function (error) {
+    logger.debug("[license_client] Error: " + error);
+
+    license_service.refreshTokenAndReconnect();
+});
+
+license_service.socket.on('connect_failed', function (error) {
+    logger.debug("[license_client] Connection Failed: " + error);
+});
+
+license_service.socket.on('connect_error', function (error) {
+    logger.debug("[license_client] Connection Error: " + error);
+});
+
+license_service.socket.on('reconnect_error', function (error) {
+    logger.debug("[license_client] Re-Connection Error: " + error);
+});
+
+license_service.socket.on('reconnect_attempt', function (number) {
+    logger.debug("[license_client] Re-Connection attempt: " + number);
+});
+
+license_service.socket.on('disconnect', function () {
+    logger.debug("disconnected from license SocketIO at Marketplace");
+});
+
+/**
+ * updateAvailable events are directly passed to the registered clients. Via the license service.
+ */
+license_service.socket.on('updateAvailable', function (data) {
+    license_service.emit('updateAvailable', data.offerId, data.hsmId);
+});
+
+
+license_service.registerUpdates = function (hsmId) {
+    /**
+     * The subscriptions for license updates are separated by the hsmId.
+     */
+    if (!hsmId) {
+        return;
+    }
+
+    license_service.registeredRooms[hsmId] = true;
+
+    license_service.socket.emit('room', hsmId);
+};
+
+license_service.unregisterUpdates = function (hsmId) {
+    if (license_service.registeredRooms[hsmId]) {
+        delete  license_service.registeredRooms[hsmId];
+    }
+
+    license_service.socket.emit('leave', hsmId);
+};
+
+license_service.socket.open();
 
 module.exports = license_service;
