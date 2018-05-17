15c3e5ffa3c56d69b7fc1b2e57d658e2769f3a80
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2018-05-17 13:03:11 +0200

    Removed some data from the models that are not processed by the clients.

--- model/recipe.js
+++ model/recipe.js
@@ -2,23 +2,16 @@
  * Created by beuttlerma on 07.02.17.
  */
 
-var Component = require('./component');
+const Component = require('./component');
 
-function Recipe(id, title, description, licenseFee, thumbnail, imageRef, backgroundColor, authorId, createdAt, updatedAt, rating, retailPrice, productCode, program, components) {
-
+function Recipe(id, title, description, licenseFee, backgroundColor, authorId, productCode, program, components) {
 
     this.id = id;
     this.title = title;
     this.description = description;
     this.licensefee = licenseFee;
-    this.thumbnail = thumbnail;
-    this.imageRef = imageRef;
     this.backgroundColor = backgroundColor;
     this.authorId = authorId;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-    this.rating = rating;
-    this.retailPrice = retailPrice;
     this.productCode = productCode;
     this.program = program;
     this.components = components;
@@ -31,12 +24,12 @@ function Recipe(id, title, description, licenseFee, thumbnail, imageRef, backgro
  * @returns {*}
  * @constructor
  */
-Recipe.prototype.CreateRecipeFromCoreJSON = function (jsonData) {
+Recipe.prototype.CreateRecipeFromCoreJSON = Recipe.CreateRecipeFromCoreJSON = function (jsonData) {
     if (!jsonData) {
         return null;
     }
-    var component = [];
-    for (var key in jsonData['componentlist']) {
+    const component = [];
+    for (let key in jsonData['componentlist']) {
         component.push(new Component().CreateComponentFromJSON(jsonData['componentlist'][key]));
     }
 
@@ -45,14 +38,8 @@ Recipe.prototype.CreateRecipeFromCoreJSON = function (jsonData) {
         jsonData['technologydataname'], //title
         jsonData['technologydatadescription'], //description
         jsonData['licensefee'], //licencefee
-        jsonData['technologydatathumbnail'], //thumbnail
-        null, //imageRef TODO: Remove imageRef from model as it is not needed. The image url always is /{id}/image
         jsonData['backgroundcolor'],
         jsonData['createdby'], //authorId
-        jsonData['createdat'], //createdAt
-        jsonData['updatedat'], //updatedAt
-        null, //rating TODO: Rating still missing
-        jsonData['retailprice'], //retailPrice
         jsonData['productcode'],
         jsonData['technologydata'],//program
         component
